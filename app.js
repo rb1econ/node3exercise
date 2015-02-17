05ce20170b8566217c980eb94e4688a651899e77
Ryan budny <rb1econ@gmail.com>
2015-02-17 16:31:10 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/success', function(req, res){
+	res.render('success');
+});
+
+app.post('/formsubmit', function(req, res){
+		res.redirect('/success');
+});
+
+var server = app.listen(8298, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
